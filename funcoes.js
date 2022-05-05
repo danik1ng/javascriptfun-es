f0f05e17fd1dfdffe2c34f43535fc15cd20b0fb9
danik1ng <102757865+danik1ng@users.noreply.github.com>
2022-05-05 15:41:38 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,41 @@
+//soma
+
+function soma (a,b){
+    console.log (a+b)
+    return
+}
+
+soma(5,3)
+
+//1
+function nome (a){
+    console.log(a)
+    return
+}
+nome('Daniel')
+
+//2
+function soma (a,b){
+    let res = a+b;
+    console.log(res)
+    return
+}
+
+soma(5,4)
+
+//3
+function multiplicação (c,d){
+    console.log(c*d)
+    return
+}
+
+multiplicação(8,9)
+
+//4
+function média(w,x,y,z){
+    let med = w+x+y+z;
+    console.log((med)/4)
+    return
+}
+
+média(1,2,3,4)
